f75d9f61bdc38ff1d3107de8e309035d9238cbd6
asallh <ajaypalsallh@gmail.com>
2025-07-29 21:55:14 -0600

    Changed the Branch Labels

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center">
       <div className="text-center">
-        <p className="mb-4 text-lg font-semibold">Branch: Main</p>
-        <Ghost size={200} mood="blissful" color="#1FC2A7" />
+        <p className="mb-4 text-lg font-semibold">Branch: Dev</p>
+        <Ghost size={200} mood="blissful" color="#515AC6" />
       </div>
     </div>
   );
