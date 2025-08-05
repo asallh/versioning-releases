15c6b7dbdfe676eb320f6a6e571065b8a62b4608
asallh <ajaypalsallh@gmail.com>
2025-08-05 12:30:06 -0600

    testing the change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center">
       <div className="text-center">
-        <p className="mb-4 text-lg font-semibold">Branch: Dev</p>
+        <p className="mb-4 text-lg font-semibold">Branch: Main</p>
         <Ghost size={200} mood="shocked" color="#FFC907" />
       </div>
         <div className="text-center text-xl">
